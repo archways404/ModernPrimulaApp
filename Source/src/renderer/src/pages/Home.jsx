be8b2fa3c00536dafb74b6f8e03414e472c64591
archways <107893165+archways404@users.noreply.github.com>
2024-02-13 15:18:39 +0100

    Modification to UI
    Changes in regards to simplification & visibility of the update button etc

--- Source/src/renderer/src/pages/Home.jsx
+++ Source/src/renderer/src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import { useNavigate } from 'react-router-dom';
-import UpdateCheck from '../components/UpdateCheck';
+import { useEffect, useState } from 'react';
+const { ipcRenderer } = window.require('electron');
 
 const Home = () => {
+	const [updateAvailable, setUpdateAvailable] = useState(false);
+
 	const navigate = useNavigate();
+
+	useEffect(() => {
+		ipcRenderer.on('update_available', () => {
+			setUpdateAvailable(true);
+		});
+
+		ipcRenderer.on('update_downloaded', () => {
+			// Code to prompt the user to install the update can go here
+		});
+
+		// Cleanup listeners when component unmounts or updates
+		return () => {
+			ipcRenderer.removeAllListeners('update_available');
+			ipcRenderer.removeAllListeners('update_downloaded');
+		};
+	}, []);
+
+	const restartApp = () => {
+		ipcRenderer.send('restart_app');
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-center pt-20 pr-10 pl-20">
 			<h1 className="text-6xl font-bold tracking-tighter mb-10">
@@ -21,31 +45,37 @@ const Home = () => {
 				<span className="text-purple-500">a</span>
 			</h1>
 			<div className="flex flex-col items-center space-y-4">
+				{updateAvailable && (
+					<button
+						className="mt-8 bg-green-500 hover:bg-green-700 text-black font-bold py-2 px-4 rounded"
+						onClick={restartApp}>
+						Update & Restart
+					</button>
+				)}
 				<button
 					onClick={() => navigate('/automatic')}
-					className="bg-red-500 text-black w-32 h-8 rounded-xl">
+					className={`bg-red-500 text-black w-32 h-8 rounded-xl ${
+						updateAvailable ? 'opacity-50 cursor-not-allowed' : ''
+					}`}
+					disabled={updateAvailable}>
 					Automatic
 				</button>
 				<button
-					onClick={() => navigate('/manual')}
-					className="bg-yellow-500 text-black w-32 h-8 rounded-full">
+					onClick={() => navigate('/home')}
+					className={`bg-yellow-500 text-black w-32 h-8 rounded-full ${
+						updateAvailable ? 'opacity-50 cursor-not-allowed' : ''
+					}`}
+					disabled={updateAvailable}>
 					Manual
 				</button>
 				<button
 					onClick={() => navigate('/settings')}
-					className="bg-green-500 text-black w-32 h-8 rounded-t-3xl">
+					className={`bg-green-500 text-black w-32 h-8 rounded-t-3xl ${
+						updateAvailable ? 'opacity-50 cursor-not-allowed' : ''
+					}`}
+					disabled={updateAvailable}>
 					Settings
 				</button>
-				<button className="bg-blue-500 text-black w-32 h-8 rounded-b-3xl">
-					Coming Soon...
-				</button>
-				<button className="bg-indigo-500 text-black w-32 h-8 rounded-tl-3xl rounded-br-3xl">
-					Coming Soon...
-				</button>
-				<button className="bg-purple-500 text-black w-32 h-8 rounded-tr-3xl rounded-bl-3xl">
-					Coming Soon...
-				</button>
-				<UpdateCheck />
 			</div>
 		</div>
 	);
--- Source/src/renderer/src/pages/Welcome.jsx
+++ Source/src/renderer/src/pages/Welcome.jsx
@@ -6,120 +6,9 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Welcome = () => {
-	const [statusMessage, setStatusMessage] = useState(null);
-	const [formData, setFormData] = useState({
-		productKey: '',
-	});
-
 	let navigate = useNavigate();
-
-	useEffect(() => {
-		const fetchMessages = async () => {
-			try {
-				const response = await fetch('http://localhost:9999/api/messages');
-				if (!response.ok) {
-					throw new Error(`HTTP error! status: ${response.status}`);
-				}
-				const data = await response.json();
-				setStatusMessage(data.Messages);
-				console.log(data.Messages);
-			} catch (error) {
-				console.error('Failed to fetch messages:', error);
-			}
-		};
-
-		fetchMessages();
-	}, []);
-
-	// Update state on input change
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-
-	// Handle form submission
-	const handleSubmit = async (e) => {
-		e.preventDefault(); // Prevent default form submission behavior
-		console.log(formData);
-		try {
-			const response = await fetch('http://localhost:9999/api/verifyKey', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(formData),
-			});
-			if (response.ok) {
-				const responseBody = await response.json();
-				console.log('Form submitted successfully:', responseBody);
-				//const isValid = responseBody.isValid
-				const productKey = responseBody.productKey;
-				const versionKey = responseBody.keyType;
-				console.log('isValid: ', isValid);
-				console.log('productKey: ', productKey);
-				console.log('versionKey: ', versionKey);
-				if (isValid) {
-					console.log('Product key verified');
-					navigate('/home');
-				} else {
-					// TEMP
-					console.log('Product not verified');
-					navigate('/home');
-				}
-				/*
-        } else {
-          toast.error('Failed', {
-            position: toast.POSITION.TOP_CENTER
-          })
-          console.log('Product key not verified')
-        }
-      */
-			} else {
-				// TEMP
-				console.log('Product not verified');
-				navigate('/home');
-				/*
-				toast.error('Failed', {
-					position: toast.POSITION.TOP_CENTER,
-				});
-				console.error('Form submission failed:', response.status);
-				// Handle error
-        */
-			}
-		} catch (error) {
-			// TEMP
-			console.log('Product not verified');
-			navigate('/home');
-			/*
-			toast.error('Failed', {
-				position: toast.POSITION.TOP_CENTER,
-			});
-			console.error('There was an error submitting the form:', error);
-			// Handle error
-      */
-		}
-	};
-
 	return (
 		<>
-			{statusMessage && (
-				<div className="w-full fixed top-0 z-50">
-					{Object.entries(statusMessage).map(([key, message]) => (
-						<div
-							key={key}
-							style={{ backgroundColor: message.color }}
-							className="p-2 text-black rounded-t mb-4 text-center">
-							<p>{message.text}</p>
-							<p className="text-gray-400 text-bold">
-								Last updated: {new Date(message.timestamp).toLocaleString()}
-							</p>
-						</div>
-					))}
-				</div>
-			)}
 			<div
 				key="1"
 				className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-center px-4">
@@ -139,25 +28,12 @@ const Welcome = () => {
 					<span className="text-indigo-500">l</span>
 					<span className="text-purple-500">a</span>
 				</h1>
-				<form
-					onSubmit={handleSubmit}
-					className="w-full max-w-md mt-4">
-					<div className="flex items-center border-b border-green-500 py-2">
-						<input
-							name="productKey"
-							value={formData.productKey}
-							onChange={handleChange}
-							className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
-							placeholder="Product Key"
-							type="text"
-						/>
-					</div>
-					<button
-						className="mt-8 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-						type="submit">
-						Proceed
-					</button>
-				</form>
+
+				<button
+					className="mt-8 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+					onClick={() => navigate('/home')}>
+					Proceed
+				</button>
 			</div>
 		</>
 	);
