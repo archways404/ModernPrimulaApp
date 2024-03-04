2f7d8dc02b1c58c7a649fa1b5d24f6942bf2f5ae
archways <107893165+archways404@users.noreply.github.com>
2024-03-04 20:07:32 +0100

    ui

--- Source/src/renderer/src/pages/Welcome.jsx
+++ Source/src/renderer/src/pages/Welcome.jsx
@@ -12,19 +12,28 @@ const Welcome = () => {
 	let navigate = useNavigate();
 	return (
 		<>
-			<div className="flex flex-col justify-between items-center min-h-screen bg-gray-800 text-center px-4 w-full">
-				<ToastContainer />
-				<div className="space-y-4">
+			<div className="flex flex-col justify-between items-center min-h-screen bg-zinc-900 text-gray-300 text-center px-4 w-full">
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
+				<div className="space-y-8 mt-20">
 					<Logo />
 					<CurrentVersion />
-					<br></br>
 					<button
-						className="mt-20 rgb-hover text-white font-bold py-2 px-4 rounded transition-colors duration-300"
+						className="mt-8 bg-indigo-600 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded-full transition ease-in-out duration-300"
 						onClick={() => navigate('/home')}>
 						Continue
 					</button>
 				</div>
-				<ToS />
+				<ToS className="mb-8" />
 			</div>
 		</>
 	);
