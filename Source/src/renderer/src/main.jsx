d1b22c285c02563677ae8ca426695b720f7959ca
archways <107893165+archways404@users.noreply.github.com>
2024-03-19 18:30:31 +0100

    Update main.jsx

--- Source/src/renderer/src/main.jsx
+++ Source/src/renderer/src/main.jsx
@@ -2,27 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
-// PRODUCTION:
-//import Welcome from './pages/Welcome.jsx';
-//import Home from './pages/Home.jsx';
-import Automatic from './pages/Automatic.jsx';
-//import Primulafunction from './pages/Primulafunction.jsx';
-import Results from './pages/Results.jsx';
-
-// DEVELOPMENT:
+// Pages
 import Welcome from './newPages/Welcome.jsx';
 import Home from './newPages/Home.jsx';
 import Settings from './newPages/Settings.jsx';
 import Ticket from './newPages/Ticket.jsx';
 import PrimulaFunction from './newPages/PrimulaFunction.jsx';
 import TicketResult from './newPages/TicketResult.jsx';
-/*
-import Home from './newPages/Home.jsx';
-import Automatic from './newPages/Automatic.jsx';
-import Primulafunction from './newPages/Primulafunction.jsx';
-import Settings from './newPages/Settings.jsx';
-import Results from './newPages/Results.jsx';
-*/
 
 import './index.css';
 
